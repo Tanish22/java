92130e13fad4cf8da8c15e7b5c55b5cdf90dceb5
Tanish22 <gupte.tanish@gmail.com>
2021-07-13 12:23:35 +0530

    Pyramid Pattern Added

--- Patterns/src/Patterns.java
+++ Patterns/src/Patterns.java
@@ -3,86 +3,99 @@ public class Patterns {
 
         // triangle
 
-        // for (int i = 1; i <= 5; i++){      
-        //     for (int j = 1 ; j <= i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= 5; i++){      
+            for (int j = 1 ; j <= i; j++){
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
 
         
         // reactangle
 
-        // for (int i = 1; i <= 6; i++){       
-        //     for (int j = 1; j <= 6; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= 6; i++){       
+            for (int j = 1; j <= 6; j++){
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
 
 
         // horizontal triangle (pointing right)
 
-        // for (int i = 1; i <= 5; i++){       
-        //     for (int j = 1; j <= i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-        // for (int i = 1; i <= 5; i++){       
-        //     for (int j = 4; j >= i; j--){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= 5; i++){       
+            for (int j = 1; j <= i; j++){
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
+        for (int i = 1; i <= 5; i++){       
+            for (int j = 4; j >= i; j--){
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
 
 
         // triangle mirror
 
-        // for (int i = 1; i <= 5; i++){       
-        //     for (int j = 5; j >= i; j--){
-        //         System.out.print(" ");
-        //     }
-        //     for (int k = 1; k <= i ; k++){
-        //         System.out.print("*");  
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= 5; i++){       
+            for (int j = 5; j >= i; j--){
+                // System.out.print(" ");
+            }
+            for (int k = 1; k <= i ; k++){
+                // System.out.print("*");  
+            }
+            // System.out.println();
+        }
 
 
         // mirror-inverse triangle
 
-        // for (int i = 1; i <= 4; i++) {
-        //     for (int j = 1; j <= i; j++){
-        //         System.out.print(" ");
-        //     }
-        //     for (int k = 4; k >= i; k--){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++){
+                // System.out.print(" ");
+            }
+            for (int k = 4; k >= i; k--){
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
 
 
         // horizontal triangle (pointing left)
 
         for (int i = 1; i <= 5; i++){       
             for (int j = 4; j >= i; j--){
-                System.out.print(" ");
+                // System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
-                System.out.print("*");
+                // System.out.print("*");
             }
-            System.out.println(); 
+            // System.out.println(); 
         }
 
         for (int i = 1; i <= 5; i++){       
             for (int j = 1; j <= i; j++){
-                System.out.print(" ");
+                // System.out.print(" ");
             }
             for (int k = 4; k >= i; k--){
-                System.out.print("*");
+                // System.out.print("*");
             }
-            System.out.println();
+            // System.out.println();
+        }
+
+
+        // pyramid pattern
+
+        for (int i = 1; i <= 5; i++){       
+            for (int j = 5; j >= i; j--){
+                // System.out.print(" ");
+            }
+            for (int k = 1; k <= i ; k++){
+               //  // System.out.print(" *");  
+            }
+            // System.out.println();
         }
     }
 }
